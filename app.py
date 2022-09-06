41d8a13b1ad484da596e69aa0b3089437a77b40a
Deep biswas <deep.biswas@codeclouds.in>
2022-09-06 15:45:28 +0530

    Dev: added gitignore

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+import sys
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+sys.dont_write_bytecode = True
 
 mongo = PyMongo(app)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from app import app
 from routes import router
+import sys
 
 app.register_blueprint(router)
+sys.dont_write_bytecode = True
 
 if __name__ == "__main__":
     app.run(debug=True)
